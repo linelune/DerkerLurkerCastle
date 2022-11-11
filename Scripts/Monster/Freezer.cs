a03835555d41765ec82ef732bf3847a9015037d7
13 :) <76404532+TherseFarah@users.noreply.github.com>
2022-11-10 22:12:45 -0500

    Prototype Clean Version

---
+++ Scripts/Monster/Freezer.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Freezer : MonoBehaviour
+{
+public bool cooldown=false;
+bool freezed=false;
+GameObject mPlayer;
+    PlayerMotor mPI;
+    // Start is called before the first frame update
+    void Start()
+    {
+    mPlayer=GameObject.FindWithTag("Player");
+    mPI=mPlayer.GetComponent<PlayerMotor>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+ public void freezePlayer()
+ {
+ cooldown=true;
+ mPI.speed=0;
+ mPI.health--;
+ Debug.Log("freeze");
+ Invoke("releasePlayer",4f);
+ Invoke("resetCoolDown",8f);
+ }
+        
+        
+        
+        
+  
+  
+  void releasePlayer()
+  {
+  mPI.speed=10;
+  Debug.Log("release");
+  }
+  
+  void resetCoolDown()
+  {
+  cooldown=false;
+  Debug.Log("reset cooldown");
+  }
+  
+  
+  
+  
+  
+  
+  
+}
---
+++ Scripts/Player/PlayerInterations.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInterations : MonoBehaviour
+{
+    public Animator animator;
+    public Collider shield;
+    public Collider sword;
+
+    private void Awake()
+    {
+        shield.enabled = false;
+        sword.enabled = false;
+    }
+    public void Attack()
+    {
+        animator.SetTrigger("OnAttack");
+        // Audio
+        FindObjectOfType<AudioManager>().Play("SwordHit", 0.2f);
+        StartCoroutine(EnableCollider(sword, 1));
+    }
+
+    public void IsDead()
+    {
+        animator.SetTrigger("IsDead");
+        //destroy game object 
+        //load main menu scene or wtvr
+    }
+
+    public void OnBlock()
+    {
+        animator.SetTrigger("IsDefend");
+        StartCoroutine(EnableCollider(shield,5));
+    }
+
+    private IEnumerator EnableCollider(Collider col, float timeInSec)
+    {
+        col.enabled = true;
+        yield return new WaitForSeconds(timeInSec);
+        col.enabled = false;
+    }
+}
